127706896a9cb314cec561892791fdde59dc557a
Michal Cech <cech.michal@gmail.com>
2016-10-11 16:05:39 +0200

    casts defined

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+
     protected $fillable = ['body', 'priority', 'done'];
+
+    protected $casts = [
+        'priority' => 'integer',
+        'done'     => 'boolean'
+    ];
 }
